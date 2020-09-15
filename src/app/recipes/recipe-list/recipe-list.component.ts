8c12f3f04bf4a5ddb3ddec9735691f985aca8b59
unknown <thaivanphu.qn95@gmail.com>
2020-09-15 23:30:04 +0700

    using ngFor outputting a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe("Chef John's Frozen Zabaglione",
+    "See how to make this easy Italian-inspired dessert—no ice cream maker required.",
+    'https://assets.visme.co/templates/banners/thumbnails/i_Easy-Cocktail-Recipes-Animated-Vertical_full.jpg',
+    '1995/02/18 12:12:12')
+  ]
   constructor() { }
 
   ngOnInit(): void {
